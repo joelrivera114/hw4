7a138a12996592d7d5ecd5d3e32e56c501bd7db9
joelrivera114 <joel.rivera74@student.qcc.cuny.edu>
2025-09-29 21:35:47 +0000

    task 2

--- hw4/IntArray.cpp
+++
@@ -1,1 +0,0 @@
-#include "IntArray.h"
--- hw4/IntArray.h
+++ hw4/IntArray.h
@@ -3,6 +3,19 @@
 #include <iostream>
 
 class IntArray {
+private:
+int *p;
+int size;
+public:
+IntArray();
+IntArray(int s);
+~IntArray();
+int getSize();
+int getValue(int index);
+void replace(int index, int value);
+void swap(int index_1, int index_2);
+void print();
+
 
 };
 
